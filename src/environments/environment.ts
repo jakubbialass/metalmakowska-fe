9e2f9b5a1595c6970ea20bd6390d3a71148ff95d
bialas.jb <kubajakub.bialas@gmail.com>
2023-05-20 18:39:15 +0200

    Changed redirect uri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@ export const environment = {
   apiUrl: 'https://metalmakowska-data-service.azurewebsites.net',
   apiScope: 'api://df3f4d27-a564-458e-ab3c-9c822c507086/User',
   clientId: '5efbd44d-6fd4-4580-9e8d-c037e7a59f23',
-  authority: 'https://login.microsoftonline.com/bialasoft.com/'//'7a3ddd7e-e55d-4703-a19b-f64e2d37c018'
+  authority: 'https://login.microsoftonline.com/bialasoft.com/', //'7a3ddd7e-e55d-4703-a19b-f64e2d37c018'
+  redirectUri: 'https://metalmakowska-fe.azurewebsites.net' // 'http://localhost:4200'
 }
