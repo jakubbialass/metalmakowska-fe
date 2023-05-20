c506e6d61e83e8dd695d0b1690270b916421f1ce
bialas.jb <kubajakub.bialas@gmail.com>
2023-05-20 18:55:39 +0200

    Changed interception

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   environmentName: 'dev',
   production: false,
-  apiUrl: 'https://metalmakowska-data-service.azurewebsites.net',
+  apiUrl: 'https://metalmakowska-data-service.azurewebsites.net', // apiUrl: ''
   apiScope: 'api://df3f4d27-a564-458e-ab3c-9c822c507086/User',
   clientId: '5efbd44d-6fd4-4580-9e8d-c037e7a59f23',
   authority: 'https://login.microsoftonline.com/bialasoft.com/', //'7a3ddd7e-e55d-4703-a19b-f64e2d37c018'
